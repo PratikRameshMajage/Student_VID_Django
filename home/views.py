2c93ea529c28140267a18924f02b70523bb7de32
Pratik Majage <89234809+PratikRameshMajage@users.noreply.github.com>
2024-05-19 10:44:22 +0530

    Finalize setup with reusable card components and updated index template

---
+++ home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def signup(request):
+    return render(request, "signup.html")
