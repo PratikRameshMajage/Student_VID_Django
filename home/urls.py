5e26b48aeb2856e334d35110e3359e2bdb04c343
Pratik Majage <89234809+PratikRameshMajage@users.noreply.github.com>
2024-05-20 10:20:28 +0530

    updating components by adding some meaningful content.

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,10 @@ from home import views
 
 urlpatterns = [
     path('', views.index, name = 'home'),
+    path('student/', views.student, name = 'student'),
+    path('card/', views.card, name = 'card'),
     path('about/', views.about, name = 'about'),
     path('contact/', views.contact, name = 'contact'),
     path('signup/', views.signup, name = 'signup'),
+    path('signin/', views.signin, name = 'signin'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,14 +5,20 @@ from django.shortcuts import render, HttpResponse
 def index(request):
     return render(request, "index.html")
 
-
 def about(request):
     return render(request, "about.html")
 
+def student(request):
+    return render(request, "student.html")
+
+def card(request):
+    return render(request, "card.html")
 
 def contact(request):
     return render(request, "contact.html")
 
-
 def signup(request):
     return render(request, "signup.html")
+
+def signin(request):
+    return render(request, "signin.html")
